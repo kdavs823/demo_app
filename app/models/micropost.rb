951d944ef2095d324e72eee9d823ee9f2a1567d4
Kyle <kdavs823@gmail.com>
2012-12-04 07:29:53 -0800

    Done with the demo app

---
+++ app/models/micropost.rb
@@ -0,0 +1,12 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :content, :user_id
+end
+
+class Micropost < ActiveRecord::Base
+validates :content, :length => { :maximum => 140 }
+end
+
+class Micropost < ActiveRecord::Base
+belongs_to :user
+validates :content, :length => { :maximum => 140 }
+end
